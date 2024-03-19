22de46e0b66564a39632a1d1b7c4f35d33dd2591
Brogoming <kotag@outlook.com>
2024-03-19 14:56:51 -0500

    updated get data from a board

--- PythonGetDatafromaBoard/main.py
+++ PythonGetDatafromaBoard/main.py
@@ -7,36 +7,11 @@ from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, Brai
 def main():
     BoardShim.enable_dev_board_logger()
 
-    parser = argparse.ArgumentParser()
-    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-    # parser.add_argument(10, type=int, help='timeout for device discovery or connection', required=False, default=0)
-    # parser.add_argument(, type=int, help='ip port', required=False, default=0)
-    # parser.add_argument(IpProtocolTypes.TCP, type=int, help='ip protocol, check IpProtocolType enum', required=False, default=0)
-    # parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
-    parser.add_argument('COM4', type=str, help='serial port', required=False, default='')
-    # parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
-    # parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
-    # parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
-    parser.add_argument(BoardIds.CYTON_DAISY_BOARD, type=int, help='board id, check docs to get a list of supported boards',
-                        required=True)
-    # parser.add_argument('--file', type=str, help='file', required=False, default='')
-    parser.add_argument(BoardIds.CYTON_DAISY_BOARD, type=int, help='master board id for streaming and playback boards',
-                        required=False, default=BoardIds.NO_BOARD)
-    args = parser.parse_args()
-
     params = BrainFlowInputParams()
-    params.ip_port = args.ip_port
-    params.serial_port = args.serial_port
-    params.mac_address = args.mac_address
-    params.other_info = args.other_info
-    params.serial_number = args.serial_number
-    params.ip_address = args.ip_address
-    params.ip_protocol = args.ip_protocol
-    params.timeout = args.timeout
-    params.file = args.file
-    params.master_board = args.master_board
+    params.serial_port = "COM4"
+    board_id = BoardIds.CYTON_DAISY_BOARD.value
 
-    board = BoardShim(args.board_id, params)
+    board = BoardShim(board_id, params)
     board.prepare_session()
     board.start_stream ()
     time.sleep(10)
